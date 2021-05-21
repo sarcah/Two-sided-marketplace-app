d08a4371efa934eb183ad77f3386b36e416ad84b
sarahcahill <sjcahill03@gmail.com>
2021-05-21 12:50:37 +1000

    adding users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root 'home#page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
