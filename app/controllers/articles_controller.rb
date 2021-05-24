1a60d2794671424637644f45e0253b0e65c67d4f
sarahcahill <sjcahill03@gmail.com>
2021-05-24 10:35:58 +1000

    fix to aws to accept credentials and images in form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,7 +65,7 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :author, :description, :price, :publication_date, :category, :thumbnail, :image)
+      params.require(:article).permit(:title, :author, :description, :price, :publication_date, :category, :user_id, :thumbnail, :image)
     end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     has_one_attached :image
+    has_one_attached :thumbnail 
+    validates :image, presence: { message: " - Every article needs to have a image for purchase" }
 end
