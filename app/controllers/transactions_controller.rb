96b0b1ac2978cfb87fbfb0227ef4d7cb0aa2f08e
sarahcahill <sjcahill03@gmail.com>
2021-05-25 12:12:28 +1000

    adding download to transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,28 +1,25 @@
 class TransactionsController < ApplicationController
     def create
+        article = Article.find params["id"]
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
-            line_items: [{
-            price_data: {
-                unit_amount: 100,
-                currency: 'aud',
-                product_data: {
-                # title: article.title,
-                # author: article.author,
-                name: 'article.title',
-                images: ['https://i.imgur.com/EHyR2nP.png'],
+            line_items: [
+            {
+                price_data: {
+                    unit_amount: article.price*100,
+                    currency: 'aud',
+                    product_data: {
+                        name: article.title
+                    },
                 },
-            },
-            quantity: 1,
+                quantity: 1,
             }],
             mode: 'payment',
-            success_url: checkout_success_url,
-            cancel_url: articles_url
+            success_url: rails_blob_url(article.image, disposition: "attachment"),
+            cancel_url: articles_url,
         })
-        
-            render json: { id: session.id }
+        render json: { id: session.id }
     end
-
     def success
     end
-end    
+end   
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
     belongs_to :user
     has_one_attached :image
     has_one_attached :thumbnail 
-    validates :image, presence: { message: " - Every article needs to have a image for purchase" }
+    validates :image, :title, :author, :price, :description, presence: true
 end
